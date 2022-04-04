0f30936260f269c0e2ab4d3d24723408810c2e89
Aashish Adhikari <a.adhikari6960@gmail.com>
2022-04-04 16:35:25 -0400

    add integerset exception

---
+++ lsp-assignments/src/org/howard/edu/lsp/assignment6/integerset/IntegerSetException.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment6.integerset;
+
+public class IntegerSetException extends Exception {
+
+	/**
+	 * @throws exception when set is empty
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * exception when set is empty
+	 */
+	public IntegerSetException(String e) {
+		super(e);
+	}
+	}
+
+
